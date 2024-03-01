1f19a80e9252ee9858ad54485dd64008337c71b8
Yesil <yesil@gmail.com>
2024-03-01 22:17:31 +0300

    searchBar

---
+++ src/components/SearchBar/SearchBar.styles.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native'
+
+export default StyleSheet.create({
+    container:{
+        backgroundColor:'#E9EAEB',
+        padding:8,
+        margin:5,
+        borderRadius:7
+
+
+    }
+
+
+})
